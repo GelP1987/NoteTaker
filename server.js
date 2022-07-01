95e870a377abae7aee5dcc96536c314d5e7298cf
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-06-30 22:21:53 -0400

    fixed landing page link btn, presentation of existing notes list, and save icon appearance while writing new notes

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 const db = require('./db/db.json');
+const fs = require("fs")
+const { note } = require('./db/db.json');
 
 app.use(express.static("public"));
 
-app.get('./notes.html', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-  app.get("./api/notes", (req, res) => {
+  app.get("/api/notes", (req, res) => {
       res.json(db);
       });
 
@@ -21,3 +23,14 @@ app.listen(PORT, () => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = note.length.toString();
+    if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const notes = createNewNote(req.body, note);
+    res.json(notes);
+  }
+});
+
